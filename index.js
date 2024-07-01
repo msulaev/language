29a3a997c7c1d4072ecf6192ebca73f7be6d4b9b
Mikhail Sulaev <49402569+msulaev@users.noreply.github.com>
2024-07-01 09:34:07 +0300

    add +  exp

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
 import assert from 'assert';
+
 /**
  * interpreter
  */
 
-class Language{
-    eval(exp){
-        if(isNumber(exp)){
+class Language {
+    eval(exp) {
+        if (isNumber(exp)) {
             return exp;
         }
-        if(isString(exp)){
+
+        if (isString(exp)) {
             return exp.slice(1, -1);
         }
+
+        if (exp[0] === '+') {
+            return exp[1] + exp[2];
+        }
+
         throw new Error('not implemented');
     }
 }
 
-function isNumber(exp){
+function isNumber(exp) {
     return typeof exp === 'number';
 }
 
-function isString(exp){
-    return typeof exp === 'string' && exp[0]=== '"' && exp[exp.length-1] === '"';
+function isString(exp) {
+    return typeof exp === 'string' && exp[0] === '"' && exp[exp.length - 1] === '"';
 }
 
 const language = new Language();
 assert.strictEqual(language.eval(1), 1);
-assert.strictEqual(language.eval("hello"), 'hello');
+assert.strictEqual(language.eval('"hello"'), 'hello');
+assert.strictEqual(language.eval(['+', 1, 5]), 6);
 
 console.log('all tests pass');
