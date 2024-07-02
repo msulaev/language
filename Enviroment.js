9b694ce35a1f02133cddb893100c70f773ea8297
Mikhail Sulaev <49402569+msulaev@users.noreply.github.com>
2024-07-02 09:52:56 +0300

    add block scopes

--- Enviroment.js
+++ Enviroment.js
@@ -1,19 +1,26 @@
+/** 
+* Scope
+*/
+
 export default class Enviroment{
 
-    constructor(record={}){
+    constructor(record = {}, parent = null) {
         this.record = record;
+        this.parent = parent;
     }
 
-    define(name, value){
+    define(name, value) {
         this.record[name] = value;
         return value;
     }
 
-    lookup(name){
-        if (!this.record.hasOwnProperty(name)){
-            throw new ReferenceError(`Variable "${name}" is not defined.`);
+    lookup(name) {
+        if (this.record.hasOwnProperty(name)) {
+            return this.record[name];
         }
-        return this.record[name];
+        if (this.parent) {
+            return this.parent.lookup(name);
+        }
+        throw new ReferenceError(`Variable "${name}" is not defined.`);
     }
 }
-
--- index.js
+++ index.js
@@ -21,23 +21,43 @@ class Language {
         }
 
         if (Array.isArray(exp) && exp[0] === '+') {
-            return this.eval(exp[1]) + this.eval(exp[2]);
+            return this.eval(exp[1], env) + this.eval(exp[2], env);
+        } 
+        
+        if (Array.isArray(exp) && exp[0] === '-') {
+            return this.eval(exp[1], env) - this.eval(exp[2], env);
         }  
         
         if (Array.isArray(exp) && exp[0] === '*') {
-            return this.eval(exp[1]) * this.eval(exp[2]);
+            return this.eval(exp[1], env) * this.eval(exp[2], env);
         }        
 
-        if (exp[0] === 'var'){
-            const[_, name, value] = exp;
-            return env.define(name, this.eval(value));
+        if (Array.isArray(exp) && exp[0] === 'var') {
+            const [_, name, value] = exp;
+            return env.define(name, this.eval(value, env));
         }
 
         if (isVariableName(exp)){
             return env.lookup(exp);
         }
 
+        // ---------------------------------------------
+        // Block: sequence of expressions
+        if (Array.isArray(exp) && exp[0] === 'begin') {
+            const blockEnv = new Enviroment({}, env);
+            return this._evalBlock(exp, blockEnv);
+        }
+
         throw new Error(`not implemented: ${JSON.stringify(exp)}`);
+    }
+
+    _evalBlock(block, env) {
+        let result;
+        const [_tag, ...expressions] = block;
+        expressions.forEach(exp => {
+            result = this.eval(exp, env);
+        });
+        return result;
     }
 }
 
@@ -53,21 +73,38 @@ function isVariableName(exp) {
     return typeof exp === 'string' && /^[a-zA-Z_$][a-zA-Z0-9_$]*$/.test(exp);
 }
 
-const language = new Language( new Enviroment({
+// ---------------------------------------------
+// Tests
+const language = new Language(new Enviroment({
     null: null,
     true: true,
     false: false,
     GLOBAL: 'global 0.1'
 }));
+
 assert.strictEqual(language.eval(1), 1);
 assert.strictEqual(language.eval('"hello"'), 'hello');
 assert.strictEqual(language.eval(['+', 1, 5]), 6);
-assert.strictEqual(language.eval(['+', ['+', 3 , 2], 5]), 10);
-assert.strictEqual(language.eval(['*', ['+', 3 , 2], 5]), 25);
+assert.strictEqual(language.eval(['+', ['+', 3, 2], 5]), 10);
+assert.strictEqual(language.eval(['*', ['+', 3, 2], 5]), 25);
 assert.strictEqual(language.eval(['var', 'x', 42]), 42);
 assert.strictEqual(language.eval('x'), 42);
 assert.strictEqual(language.eval(['var', 'y', 'true']), true);
+assert.strictEqual(language.eval(
+    ['begin',
+        ['var', 'x', 10],
+        ['var', 'y', 20],
+        ['+', 'x', 'y']
+    ]), 30);
 
-
+assert.strictEqual(language.eval(
+    ['begin',
+        ['var', 'x', 10],
+        ['begin',
+            ['var', 'x', 20],
+            'x'
+        ],
+        'x'
+    ]), 10);
 
 console.log('all tests pass');
